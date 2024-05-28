5b05ea5feda9d4015fb675b2d6ab6abf799302cc
Matheus <matheus.resteves@gmail.com>
2024-05-28 13:53:13 -0300

    fix: remove embraerLogo variable

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let airplane = document.getElementById('airplane');
-let embraerLogo = document.getElementById('logo');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    embraerLogo.style.top = value + 'px';
     airplane.style.left = value * -1.5 + 'px';
 });
